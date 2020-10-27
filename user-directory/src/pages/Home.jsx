531ff4c653d11e75af921d217e82ffea2e6c5fcc
Cristian Hornedo <officialcristianl@gmail.com>
2020-10-26 21:51:53 -0400

    Added more feature to home page to give it the information from the json files

--- user-directory/src/pages/Home.jsx
+++ user-directory/src/pages/Home.jsx
@@ -1,10 +1,29 @@
-import React from "react"
+import React from "react";
+import { AllEmployees } from "../components/AllEmployees";
+import Employees from "../seed/randomEmps.json";
+import "./styles.css";
 
 export default function Home() {
     return (
+        // If there is no filter by prop or sort cate then load All Employees normally
         <div className="container" >
-            <h1>Home page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores quisquam porro vel at, nostrum in dolor commodi deleniti magni dolore reprehenderit voluptatem natus corporis. Labore totam ipsa facilis veritatis ratione!</p>
+            <form >
+                
+            </form>
+
+            
+            <h1>User Directory</h1>
+            <div className="row">
+                {Employees.map(employee => (
+                    <AllEmployees {...employee}/>
+                ))}
+            </div>
+            
         </div>
+
+        // If their is a filter then load based on filter
+
+        // If their is a sort by cate then load based on sort
+
     )
 }
