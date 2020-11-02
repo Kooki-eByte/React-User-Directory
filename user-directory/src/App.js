d2cfef42ef6b9846d718a8e5e9e5e8d79135d4ec
Cristian Hornedo <officialcristianl@gmail.com>
2020-11-02 17:26:51 -0500

    Finished the sort and filter functionality. Going to try and break these two apart into some other components

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -7,7 +7,6 @@ import Home from "./pages/Home";
 TODO - Sort the table by at least one category (Have a form that listens and calls a component to Sort the array of objects based on category given)
 
 TODO - In the form listen for the filter (maybe a select option of a given property, such as work, department)
-
 */
 
 function App() {
@@ -15,6 +14,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>User Employee Directory</h1>
+        <br />
         <Router>
           <Route exact path="/" component={Home} />
         </Router>
